8ef833fe3b5df846c1f346b4895d800516fdf6b7
Charlie Zhao <chuiniube@qq.com>
2024-06-27 10:34:12 +0000

    update

--- 02_start_generation.py
+++ 02_start_generation.py
@@ -1,18 +1,19 @@
 import argparse
 from glob import glob
-from tqdm import tqdm
 import os
 import numpy as np
 import xml.etree.ElementTree as ET
-from random import choices, sample
-from utils import crossover, find_crossover, find_fitness, find_if_in_list, find_mutation, find_population_by_mutation, find_unique_xmls, mutation, find_size_by_name, rotate_islands, sample_tree_from_files
+from utils import find_crossover, find_fitness, find_mutation, find_population_by_mutation, find_unique_xmls, rotate_islands
 from torch.utils.tensorboard import SummaryWriter
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-t", "--Template", help="the template path")
-argParser.add_argument("-s", "--ShrinkFrame", action='store_true', help="shrink the frame in mutation")
-argParser.add_argument("-c", "--TotalPices", type=int, default=100,  help="total piece of population to generate")
-argParser.add_argument("-i", "--IslandNumber", type=int, default=10, help="total island number")
+argParser.add_argument("-s", "--ShrinkFrame",
+                       action='store_true', help="shrink the frame in mutation")
+argParser.add_argument("-c", "--TotalPices", type=int,
+                       default=100,  help="total piece of population to generate")
+argParser.add_argument("-i", "--IslandNumber", type=int,
+                       default=10, help="total island number")
 
 args = argParser.parse_args()
 
@@ -28,7 +29,8 @@ island_list = []
 
 for i in range(args.IslandNumber):
     # xml_list = sample_tree_from_files(ori_xml_files, k=100)
-    xml_list = find_population_by_mutation(ET.parse(args.Template), args.Template, args.ShrinkFrame, 100)
+    xml_list = find_population_by_mutation(
+        ET.parse(args.Template), args.Template, args.ShrinkFrame, 100)
     print(f'initial island {i} generated')
     fitness_list = find_fitness(xml_list)
     island_list.append({
@@ -51,23 +53,24 @@ while True:
             elite_xmls.append(xml_list[index])
         island_dict['elite'] = elite_xmls
         elite_group_list.append(elite_xmls[0])
-    
+
     elite_fitness_list = find_fitness(elite_group_list)
     elite_xml = elite_group_list[np.argmax(elite_fitness_list)]
     elite_xml.write(f'./results/{iteration}.xml')
-    
-    
+
     if iteration % 20 == 0:
         island_list = rotate_islands(island_list)
-    
+
     max_level_list = []
     for i in range(args.IslandNumber):
         island_dict = island_list[i]
         fitness_list = island_dict['fitness']
         xml_list = island_dict['xml']
         elite_xmls = island_dict['elite']
-        offspring_list = find_crossover(xml_list, fitness_list, args.TotalPices, args.Template)
-        offspring_list = find_mutation(offspring_list, args.Template, args.ShrinkFrame)
+        offspring_list = find_crossover(
+            xml_list, fitness_list, args.TotalPices, args.Template)
+        offspring_list = find_mutation(
+            offspring_list, args.Template, args.ShrinkFrame)
         xml_list = elite_xmls + offspring_list
         xml_list = find_unique_xmls(xml_list)
         fitness_list = find_fitness(xml_list)
@@ -76,13 +79,5 @@ while True:
         max_level_list.append(max(fitness_list))
     tb.add_scalar(f"MaxLevels", max(max_level_list), iteration)
     tb.add_scalar(f"MinLevels", min(max_level_list), iteration)
-    
-    # elite_xmls[0].write(f'./results/{iteration}.xml')
 
-    
-    
-    
-    
-    
-    
-    
+    # elite_xmls[0].write(f'./results/{iteration}.xml')
